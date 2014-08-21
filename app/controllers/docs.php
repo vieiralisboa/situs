ad56d9c87017a9ad64cbf59496a3149a8d3a453b
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-08-21 23:07:56 +0100

    Update Docs controller

--- app/controllers/docs.php
+++ app/controllers/docs.php
@@ -12,29 +12,25 @@ class Docs_Controller {
         });
 
         Router::route('/docs/bar/templates/:template', function($request) {
-            $HTDOCS = "/shares/jlisboa/WD SmartWare.swstor/ULTRABOOK/Volume.7d313caf.1c93.4c09.838c.44ab9c4ba2a0/htdocs";
-            $file = $HTDOCS."/sites/ze/js/bar/templates/".$request->data['template'];
+            $file = Router::$controller_config->htdocs."/sites/ze/js/bar/templates/".$request->data['template'];
             if(!file_exists($file)) Util::quit(404);
             return Util::serve($file);
         });
 
         Router::route('/docs/bar/css/:sheet', function($request) {
-            $HTDOCS = "/shares/jlisboa/WD SmartWare.swstor/ULTRABOOK/Volume.7d313caf.1c93.4c09.838c.44ab9c4ba2a0/htdocs";
-            $file = $HTDOCS."/sites/ze/js/bar/css/".$request->data['sheet'];
+            $file = Router::$controller_config->htdocs."/sites/ze/js/bar/css/".$request->data['sheet'];
             if(!file_exists($file)) Util::quit(404);
             return Util::download($file);
         });
 
         Router::route('/docs/bar/:file', function($request) {
-            $HTDOCS = "/shares/jlisboa/WD SmartWare.swstor/ULTRABOOK/Volume.7d313caf.1c93.4c09.838c.44ab9c4ba2a0/htdocs";
-            $file = $HTDOCS."/sites/ze/js/bar/js/".$request->data['file'];
+            $file = Router::$controller_config->htdocs."/sites/ze/js/bar/js/".$request->data['file'];
             if(!file_exists($file)) Util::quit(404);
             return Util::download($file);
         });
 
         Router::route('/docs/uploader/:file', function($request) {
-            $HTDOCS = "/shares/jlisboa/WD SmartWare.swstor/ULTRABOOK/Volume.7d313caf.1c93.4c09.838c.44ab9c4ba2a0/htdocs";
-            $file = $HTDOCS."/sites/ze/public/upload/".$request->data['file'];
+            $file = Router::$controller_config->htdocs."/sites/ze/public/upload/".$request->data['file'];
             if(!file_exists($file)) Util::quit(404);
             return Util::download($file);
         });
