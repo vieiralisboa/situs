15d85517e487c7d9dedc444e16ddef90456d9bf2
vieiralisboa <vieira.lisboa@yahoo.com>
2014-09-01 16:47:57 +0100

    Update _ controller

--- app/controllers/_.php
+++ app/controllers/_.php
@@ -7,7 +7,7 @@ class __Controller {
      */
     public function get() {
         Router::route('/_', function() {
-            $_ = "c:\\temp\\underscore-min.js";
+            $_ = Router::$controller_config->temp . "underscore-min.js";
             $data = file_get_contents("http://docs.medorc.org/underscore/1.4.2/underscore-min.js");
             file_put_contents($_, $data);
             Util::serve($_);
