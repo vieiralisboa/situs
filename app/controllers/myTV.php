da13be646134f3e0a86373882377bb56bf28b893
vieiralisboa <vieira.lisboa@yahoo.com>
2014-09-06 18:07:44 +0100

    Update myTV controller

--- app/controllers/myTV.php
+++ app/controllers/myTV.php
@@ -1,8 +1,7 @@
 <?php
 
 /**
- * Upload/Stream Video
- * TODO seperate controllers for folder contents and download_video
+ * myTV controller
  */
 class MyTV_Controller {
 
@@ -15,11 +14,6 @@ class MyTV_Controller {
 
         // Routes
         switch($request->uri[1]){
-
-            case 'popup':
-                $file = Router::$controller_config->popup;
-                if(!file_exists($file)) Util::quit(404);
-                return Util::serve($file);
 
             // Route /myTV/config
             case 'config':
@@ -36,7 +30,20 @@ class MyTV_Controller {
                 $path .= $sep.$dir;
 
                 $videos = array();
-                foreach(scandir($path) as $video){
+                $_dir = opendir($path);
+                $_list = array();
+                while($_file = readdir($_dir)){
+                    if ($_file != '.' and $_file != '..'){
+                        // add the filename, to be sure not to
+                        // overwrite a array key
+                        $_ctime = filectime($path .$sep. $_file) . ',' . $_file;
+                        $_list[$_ctime] = $_file;
+                    }
+                }
+                closedir($_dir);
+                krsort($_list);
+
+                foreach($_list as $video){
                     $info = pathinfo($video);
                     if(preg_match('/\.mp4$/', $video)) {
                         $name = $info['filename'];
@@ -46,7 +53,7 @@ class MyTV_Controller {
                         $name = str_replace('.', " ", $name);
                         $show = array(
                             'name' => $name,// $info['basename'],
-                            'mp4' => 'show'.$sep.$dir.$sep.$info['filename']
+                            'mp4' => 'video'.$sep.$dir.$sep.$info['filename']
                             //'mp4' => "http://guest:guest@situs.no-ip.org:8080/myTV/show/" . $dir."/".$info['filename']
                         );
 
@@ -57,9 +64,9 @@ class MyTV_Controller {
 
                         // Poster
                         if(file_exists($path.$sep.$info['filename'].".jpg"))
-                            $show['poster'] = "/myTV/poster/".$dir.$sep.$info['filename'].".jpg";
+                            $show['poster'] = "poster/".$dir.$sep.$info['filename'].".jpg";
                         elseif (file_exists($path.$sep.$info['filename'].".png"))
-                            $show['poster'] = "/myTV/poster/".$dir.$sep.$info['filename'].".jpg";
+                            $show['poster'] = "poster/".$dir.$sep.$info['filename'].".jpg";
                         else $show['poster'] = false;
 
                         // add video to the list
@@ -68,163 +75,8 @@ class MyTV_Controller {
                 }
                 return $videos;
 
-            // Video poster
-            case 'poster':
-                $file = $config->path;
-                while(isset($request->uri[++$k]))
-                    $file .= $sep.$request->uri[$k];
-
-                //return $file;
-
-                if(!file_exists($file)) Util::quit(404);
-                Util::serve($file);
-                return;
-
-            // Route /myTV/show/$file
-            case 'show':
-                // path to the file's folder
-                $file = $config->path;
-                while(isset($request->uri[++$k]))
-                    $file .= $sep.$request->uri[$k];
-
-                //$filename = $request->uri[$k];
-
-                // requires curl
-                #if(file_exists($file)) mp4Upload($file);
-
-                // requested video // only stream partial video (range)
-                if(!file_exists($file) || empty($_SERVER['HTTP_RANGE']))
-                    return false;
-
-                rangeDownload($file);// stream the video
-                exit;
-
             default:
                 return false;
         }
     }
-}
-
-function mp4Upload($mp4){
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $mp4);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    $out = curl_exec($ch);
-    curl_close($ch);
-
-    header('Content-type: video/mp4');
-    header('Content-type: video/mpeg');
-    header('Content-disposition: inline');
-    header("Content-Transfer-Encoding:­ binary");
-    header("Content-Length: ".filesize($out));
-    echo $out;
-    exit;
-}
-
-/**
- * Download file range
- */
-function rangeDownload($file) {
-    $fp = @fopen($file, 'rb');
-
-    $size   = filesize($file); // File size
-    $length = $size;           // Content length
-    $start  = 0;               // Start byte
-    $end    = $size - 1;       // End byte
-
-    // Now that we've gotten so far without errors we send the accept range header
-
-    // set mp4 mime-type header
-    header("Content-Type: video/mp4");
-
-    /* At the moment we only support single ranges.
-     * Multiple ranges requires some more work to ensure it works correctly
-     * and comply with the spesifications: http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2
-     *
-     * Multirange support annouces itself with:
-     * header('Accept-Ranges: bytes');
-     *
-     * Multirange content must be sent with multipart/byteranges mediatype,
-     * (mediatype = mimetype)
-     * as well as a boundry header to indicate the various chunks of data.
-     */
-    header("Accept-Ranges: 0-$length");
-    // header('Accept-Ranges: bytes');
-    // multipart/byteranges
-    // http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2
-    if (isset($_SERVER['HTTP_RANGE'])) {
-        $c_start = $start;
-        $c_end   = $end;
-
-        // Extract the range string
-        list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
-
-        // Make sure the client hasn't sent us a multibyte range
-        if (strpos($range, ',') !== false) {
-            // (?) Shoud this be issued here, or should the first
-            // range be used? Or should the header be ignored and
-            // we output the whole content?
-            header('HTTP/1.1 416 Requested Range Not Satisfiable');
-            header("Content-Range: bytes $start-$end/$size");
-            // (?) Echo some info to the client?
-
-            // DEBUG
-            //file_put_contents("myTV_416_headers.json", json_encode(getallheaders()));
-
-            exit;
-        }
-
-        // If the range starts with an '-' we start from the beginning
-        // If not, we forward the file pointer
-        // And make sure to get the end byte if spesified
-        if ($range[0] == '-') {
-
-            // The n-number of the last bytes is requested
-            $c_start = $size - substr($range, 1);
-        }
-        else {
-            $range  = explode('-', $range);
-            $c_start = $range[0];
-            $c_end   = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
-        }
-
-        /* Check the range and make sure it's treated according to the specs.
-         * http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
-         */
-
-        // End bytes can not be larger than $end.
-        $c_end = ($c_end > $end) ? $end : $c_end;
-
-        // Validate the requested range and return an error if it's not correct.
-        if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
-            header('HTTP/1.1 416 Requested Range Not Satisfiable');
-            header("Content-Range: bytes $start-$end/$size");
-            // (?) Echo some info to the client?
-            exit;
-        }
-        $start  = $c_start;
-        $end    = $c_end;
-        $length = $end - $start + 1; // Calculate new content length
-        fseek($fp, $start);
-        header('HTTP/1.1 206 Partial Content');
-    }
-
-    // Notify the client the byte range we'll be outputting
-    header("Content-Range: bytes $start-$end/$size");
-    header("Content-Length: $length");
-
-    // Start buffered download
-    $buffer = 1024 * 8;
-    while(!feof($fp) && ($p = ftell($fp)) <= $end) {
-        if ($p + $buffer > $end) {
-            // In case we're only outputtin a chunk, make sure we don't read past the length
-            $buffer = $end - $p + 1;
-        }
-        set_time_limit(0); // Reset time limit for big files
-        echo fread($fp, $buffer);
-        flush(); // Free up memory. Otherwise large files will trigger PHP's memory limit.
-    }
-
-    fclose($fp);
 }
