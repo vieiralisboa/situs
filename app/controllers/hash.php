7c4fdc6926b4f833dc725945e64ce576fba3518f
vieiralisboa <vieira.lisboa@yahoo.com>
2014-09-11 17:00:52 +0100

    Update Hash controller

--- app/controllers/hash.php
+++ app/controllers/hash.php
@@ -2,37 +2,43 @@
 
 /**
  * Hash
- * String and password hashing
+ * String hashing
  */
 class Hash_Controller {
 
     public function get($request) {
 
         Router::route('/hash', function($request){
-            //return $request;
+            return [
+                "routes" => [
+                    "/hash/:string",
+                    "/hash/:string/:algo",
+                    "/hash/:string/:algo/:cost"],
+                "algos"=>hash_algos()
+
+            ];//$request;
         });
 
-        Router::route('/hash/password/:password', function($request){
-            //TODO validate
-            return Util::zehash($request->data['password'], $_SERVER['REMOTE_ADDR']);
+        Router::route('/hash/:string', function($request){
+            $algo = "sha1";
+            return array(
+                "string" => $request->data['string'],
+                "algorithm" => $algo,
+                "hash" => hash($algo, $request->data['string'])
+            );
         });
 
-        Router::route('/hash/password/:password/:salt', function($request){
-            //TODO validate
-            return Util::zehash($request->data['password'], $request->data['salt']);
-        });
-
-        Router::route('/hash/:algo/:string', function($request){
+        Router::route('/hash/:string/:algo', function($request){
             $algo = null;
 
-            foreach(["md5", "sha1", "sha256", "sha512", "ripemd128"] as $mode){
+            foreach(hash_algos() as $mode){
                 if($request->data['algo'] == $mode) {
                     $algo = $mode;
                     break;
                 }
             }
 
-            $algo = $algo ? $algo : "md5";
+            $algo = $algo ? $algo : "sha1";
 
             return array(
                 "string" => $request->data['string'],
@@ -41,9 +47,33 @@ class Hash_Controller {
             );
         });
 
-        Router::route('/hash/verify/:password/:hash', function($request){
-            //TODO validate
-            return Util::zehash_verify($request->data['password'], $request->data['hash']);
+        Router::route('/hash/:string/:algo/:cost', function($request){
+            $algo = null;
+            $cost = 0;
+
+            foreach(hash_algos() as $mode){
+                if($request->data['algo'] == $mode) {
+                    $algo = $mode;
+                    break;
+                }
+            }
+
+            $algo = $algo ? $algo : "sha1";
+            $cost = (int) $request->data['cost'];
+            if($cost < 1) $cost = 1;
+            if($cost > 65536) $cost = 65536;
+
+            $hash = hash($algo, $request->data['string']);
+            for($i=1; $i<$cost; $i++){
+                $hash = hash($algo, $hash);
+            }
+
+            return array(
+                "string" => $request->data['string'],
+                "algorithm" => $algo,
+                "passes" => $cost,
+                "hash" => $hash
+            );
         });
     }
 }
