c35550d3a992cf2ed834486f4d6d24da06b47e15
vieiralisboa <vieira.lisboa@yahoo.com>
2014-09-14 18:51:00 +0100

    Update myTV controller

--- app/controllers/myTV.php
+++ app/controllers/myTV.php
@@ -24,39 +24,41 @@ class MyTV_Controller {
                 if(!isset($request->uri[$k+1])) return false;
 
                 $path = $config->path;
-                $dir = $request->uri[++$k];
+                $dir = $root = $request->uri[++$k];
                 while(isset($request->uri[++$k]))
                     $dir .=  $sep.$request->uri[$k];
                 $path .= $sep.$dir;
 
-                $videos = array();
-                $_dir = opendir($path);
-                $_list = array();
-                while($_file = readdir($_dir)){
-                    if ($_file != '.' and $_file != '..'){
-                        // add the filename, to be sure not to overwrite a array key
-                        $_ctime = filectime($path.$sep.$_file) . ',' . $_file;
-                        $_list[$_ctime] = $_file;
+                $videos = $_list = array();
+
+                if(file_exists($path)) {
+                    $_dir = opendir($path);
+                    while($_file = readdir($_dir)){
+                        if ($_file != '.' and $_file != '..'){
+                            if($root != "tv-shows") {
+                                // add the filename, to be sure not to overwrite a array key
+                                $_ctime = filectime($path.$sep.$_file) . ',' . $_file;
+                                $_list[$_ctime] = $_file;
+                            }
+                            else {
+                                $_ctime = $_file;
+                                $_list[$_ctime] = $_file;
+                            }
+                        }
                     }
+                    closedir($_dir);
+                    if($root != "tv-shows") krsort($_list);
+                    else sort($_list);
                 }
-                closedir($_dir);
-                krsort($_list);
 
                 foreach($_list as $video){
                     $info = pathinfo($video);
-                    if(preg_match('/\.mp4$/', $video)) {
-                        $name = $info['filename'];
-                        $name = preg_replace("/\.hdtv.*/i", "", $name);
-                        $name = preg_replace("/\.proper.*/i", "", $name);
-                        $name = preg_replace("/\.rerip.*/i", "", $name);
-                        $name = preg_replace("/\.webrip.*/i", "", $name);
-                        $name = preg_replace("/\.web\-dl.*/i", "", $name);
-                        $name = str_replace('.', " ", $name);
 
-                        $show = array(
-                            'name' => $name,// $info['basename'],
-                            'mp4' => 'video'.$sep.$dir.$sep.$info['filename']
-                        );
+                    if(preg_match('/\.mp4$/', $video)) {
+                        $show = array();
+
+                        $show['mp4'] = 'video'.$sep.$dir.$sep.$info['filename'];
+                        $show['name'] = str_replace('.', " ", $info['filename']);
 
                         // Subtitles (vtt)
                         if(file_exists($path.$sep.$info['filename'].".PT.srt")){
@@ -64,21 +66,36 @@ class MyTV_Controller {
                         }
 
                         // Poster
-                        if(file_exists($path.$sep.$info['filename'].".jpg"))
-                            $show['poster'] = "poster/".$dir.$sep.$info['filename'].".jpg";
-                        elseif (file_exists($path.$sep.$info['filename'].".png"))
-                            $show['poster'] = "poster/".$dir.$sep.$info['filename'].".jpg";
-                        else {
+                        if($root == "tv-shows" || $root == "NEW"){
+                            $name = $info['filename'];
+                            $name = preg_replace("/\.hdtv.*/i", "", $name);
+                            $name = preg_replace("/\.proper.*/i", "", $name);
+                            $name = preg_replace("/\.rerip.*/i", "", $name);
+                            $name = preg_replace("/\.webrip.*/i", "", $name);
+                            $name = preg_replace("/\.web\-dl.*/i", "", $name);
+                            $name = str_replace('.', " ", $name);
+
+                            $show['name'] = $name;
+
                             $pattern = "/ s\d{1,2}e\d{1,2}/i";
-                            if(preg_match($pattern, $name, $matches)){
+
+                            if(preg_match($pattern, $name)){
                                 $name = preg_replace("/ s\d{1,2}e\d{1,2}/i", "", $name);
                                 $name = str_replace(' ', "_", $name);
                                 $file = "tv-shows/".$name.$sep.$name.".jpg";
+
                                 if(file_exists($config->path.$sep.$file)){
                                     $show['poster'] = "poster/".$file;
                                 }
                                 else $show['poster'] = false;
                             }
+                            else $show['poster'] = false;
+                        }
+                        else {
+                            if(file_exists($path.$sep.$info['filename'].".jpg"))
+                                $show['poster'] = "poster/".$dir.$sep.$info['filename'].".jpg";
+                            #elseif (file_exists($path.$sep.$info['filename'].".png"))
+                            #    $show['poster'] = "poster/".$dir.$sep.$info['filename'].".png";
                             else $show['poster'] = false;
                         }
 
