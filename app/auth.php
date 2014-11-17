2c514dc902424f6a886f54e346481aafdb90fb3d
vieiralisboa <vieira.lisboa@yahoo.com>
2014-11-17 17:37:43 +0000

    Update Auth

--- app/auth.php
+++ app/auth.php
@@ -11,62 +11,88 @@
 /**
  * Authorization
  */
-class Auth extends Util{
+class Auth extends Util
+{
 
-	// dummy db
-	public static function db($user, $realm){
-		#$file = dirname(__FILE__)."\auth\\$realm.json";
-		$file = dirname(__FILE__)."/auth/$realm.json";
-		if(file_exists($file)){
-			$db = json_decode(file_get_contents($file));
-			foreach($db as $record)
-				if($record->user == $user) return $record->pw;
-		}
-		//else die($file);
+    // dummy db
+    public static function db($user, $realm)
+    {
+        $file = dirname(__FILE__)."/auth/$realm.json";
+        if (file_exists($file)) {
+            $db = json_decode(file_get_contents($file));
+            foreach($db as $record) {
+                if($record->user == $user) return $record->pw;
+            }
+        }
+        //else die($file);
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 *
-	 */
-	public static function unauthorize($realm){
-	    header("WWW-Authenticate: Basic realm=\"$realm\"");
-	    self::quit(401);
-	}
+    /**
+     *
+     */
+    public static function user()
+    {
+        // no username
+        if (empty($_SERVER['PHP_AUTH_USER'])) return null;
 
-	/**
-	 * Basic Authorization
-	 * Realm is Api name http://host/<Api>/:some/:more
-	 */
-	public static function basic($realm){
-		// list of controllers that do not require authorization
-		#$ignore = dirname(__FILE__)."\auth\.ignore.json";
-		$ignore = dirname(__FILE__)."/auth/.ignore.json";
-		if(file_exists($ignore)){
-			$controllers = json_decode(file_get_contents($ignore));
-			foreach($controllers as $controller) {
-				if($controller == $realm) {
-					return;
-				}
-			}
-		}
+        return $_SERVER['PHP_AUTH_USER'];
+    }
 
-		// no username or password
-		if (empty($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_PW'])) {
-			self::unauthorize($realm);
-		}
+    /**
+     *
+     */
+    public static function pw()
+    {
+        // no password
+        if (empty($_SERVER['PHP_AUTH_PW'])) return null;
 
-		// get user password from the database
-		//"SELECT pw FROM $realm WHERE user='{$_SERVER['PHP_AUTH_USER']}'";
+        return $_SERVER['PHP_AUTH_PW'];
+    }
 
-		$pw = self::db(strtolower($_SERVER['PHP_AUTH_USER']), $realm);
+    /**
+     *
+     */
+    public static function unauthorize($realm)
+    {
+        header("WWW-Authenticate: Basic realm=\"$realm\"");
+        self::quit(401);
+    }
 
-		$auth = Util::zehash_verify($_SERVER['PHP_AUTH_PW'], $pw);
+    /**
+     * Basic Authorization
+     * Realm is Api name http://host/<Api>/:some/:more
+     */
+    public static function basic($realm)
+    {
+        // list of controllers that do not require authorization
+        #$ignore = dirname(__FILE__)."\auth\.ignore.json";
+        $ignore = dirname(__FILE__)."/auth/.ignore.json";
+        if (file_exists($ignore)) {
+            $controllers = json_decode(file_get_contents($ignore));
+            foreach($controllers as $controller) {
+                if($controller == $realm) {
+                    return;
+                }
+            }
+        }
 
-		// password verification
-		if ($auth['verifies'] != true) {
-			self::unauthorize($realm);
-		}
-	}
+        // no username or password
+        if (empty($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_PW'])) {
+            self::unauthorize($realm);
+        }
+
+        // get user password from the database
+        //"SELECT pw FROM $realm WHERE user='{$_SERVER['PHP_AUTH_USER']}'";
+
+        $pw = self::db(strtolower($_SERVER['PHP_AUTH_USER']), $realm);
+
+        $auth = Util::zehash_verify($_SERVER['PHP_AUTH_PW'], $pw);
+
+        // password verification
+        if ($auth['verifies'] != true) {
+            self::unauthorize($realm);
+        }
+    }
 }
