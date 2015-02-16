2030f8e3fe822adf8f73a784ac2cef3b578fdac7
vieiralisboa <vieira.lisboa@yahoo.com>
2015-02-16 18:40:38 +0000

    Delete damas controller

--- app/controllers/damas.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Damas_Controller {
-    public function get() {
-        Util::quit(401);
-        //return Util::download("\\\\MYBOOKLIVE\\Media\\ebooks\\dump\\COMPUTER\\programming\\C\\Damas.pdf");
-    }
-}
